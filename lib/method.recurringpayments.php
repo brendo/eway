c4fe47e93c244a993c843ec998f1439f6b5b0cfc
André Bocati <andre@randb.com.au>
2013-03-21 15:04:08 +1000

    Fixed wrong URL for live environment.

--- lib/method.recurringpayments.php
+++ lib/method.recurringpayments.php
@@ -7,13 +7,13 @@
         public static function getGatewayURI() {
             return (eWayAPI::isTesting())
                 ? 'https://www.eway.com.au/gateway/rebill/test/managerebill_test.asmx'
-                : 'https://www.eway.com.au/gateway/rebill/upload.aspx';
+                : 'https://www.eway.com.au/gateway/rebill/manageRebill.asmx';
         }
 
         public static function getDefaults() {}
 
         public static function getRequiredFields() {}
-        
+
         /**
          * Required fields to create a rebill customer.
          * @return type
@@ -26,25 +26,25 @@
                 'customerEmail'
             );
         }
-       
+
         /**
          * Required fields to create a rebill event.
          * @return type
          */
         public static function getRequiredCreateRebillEvent() {
             return array(
-                'RebillCustomerID', 
-                'RebillCCName',     
-                'RebillCCNumber',   
-                'RebillCCExpMonth',     
-                'RebillCCExpYear',  
-                'RebillInitAmt',        
-                'RebillInitDate',   
-                'RebillRecurAmt',   
-                'RebillStartDate',  
-                'RebillInterval',   
+                'RebillCustomerID',
+                'RebillCCName',
+                'RebillCCNumber',
+                'RebillCCExpMonth',
+                'RebillCCExpYear',
+                'RebillInitAmt',
+                'RebillInitDate',
+                'RebillRecurAmt',
+                'RebillStartDate',
+                'RebillInterval',
                 'RebillIntervalType',
-                'RebillEndDate',        
+                'RebillEndDate',
             );
         }
 
@@ -54,39 +54,39 @@
          */
         public static function getRequiredUpdateRebillEvent() {
             return array(
-                'RebillCustomerID', 
-                'RebillID', 
-                'RebillCCName',     
-                'RebillCCNumber',   
-                'RebillCCExpMonth',     
-                'RebillCCExpYear',  
-                'RebillInitAmt',        
-                'RebillInitDate',   
-                'RebillRecurAmt',   
-                'RebillStartDate',  
-                'RebillInterval',   
+                'RebillCustomerID',
+                'RebillID',
+                'RebillCCName',
+                'RebillCCNumber',
+                'RebillCCExpMonth',
+                'RebillCCExpYear',
+                'RebillInitAmt',
+                'RebillInitDate',
+                'RebillRecurAmt',
+                'RebillStartDate',
+                'RebillInterval',
                 'RebillIntervalType',
-                'RebillEndDate',        
+                'RebillEndDate',
             );
         }
-        
+
     }
 
     Class RecurringPayments extends Recurring_Request {
-       
+
         /**
          * Create a new Rebill Customer.
-         * 
+         *
          * @param array $values Array of values containing the Customer's details to create a new rebill customer.
-         * 
+         *
          * @return RebillCustomerID
          */
         public static function createRebillCustomer(array $values = array()) {
-            
+
             // Check for missing fields
             $valid_data = true;
             $missing_fields = array();
-            $error = null;                
+            $error = null;
             foreach (RecurringPaymentsSettings::getRequiredCreateRebillCustomer() as $field_name) {
                 if (!array_key_exists($field_name, $values) || $values[$field_name] == '') {
                     $missing_fields[] = $field_name;
@@ -102,19 +102,19 @@
                     'response-message' => __('Missing Fields: %s', array(implode(', ', $missing_fields))),
                     'missing-fields' => $missing_fields
                 );
-            }              
+            }
 
             $eway_request_xml = simplexml_load_string('<CreateRebillCustomer xmlns="http://www.eway.com.au/gateway/rebill/manageRebill" />');
             foreach($values as $field_name => $field_data) {
                 $eway_request_xml->addChild($field_name, General::sanitize($field_data));
             }
-            
+
             // Execute the transaction.
             $ch = Recurring_Request::start(RecurringPaymentsSettings::getGatewayURI(), $eway_request_xml->asXML());
 
             $curl_result = curl_exec($ch);
             $status = curl_getinfo($ch);
-            
+
             if (curl_errno($ch)) {
                 // The Gateway did not connect to eWay successfully or some error occurred.
                 return array(
@@ -123,9 +123,9 @@
                     'response-message' => __('There was an error connecting to eWay.'),
                     'curl-info' => $info
                 );
-            } 
+            }
             else {
-                
+
                 curl_close($ch);
 
                 $dom = new DOMDocument();
@@ -143,7 +143,7 @@
 
                 $customerId = $xpath->evaluate('string(/soap:Envelope/soap:Body//eway:RebillCustomerID)');
                 $result = $xpath->evaluate('string(/soap:Envelope/soap:Body//eway:Result)');
-                
+
                 // Return a new rebill customer id.
                 if ($result == "Success") {
                     return $customerId;
@@ -154,28 +154,28 @@
                         'response-message' => __('There was an error creating a new reBill customer.'),
                         'curl-info' => $status
                     );
-                }                    
+                }
             }
         }
-               
+
         /**
          * Create a new Rebill Event.
          *
          * @param array $values
-         * 
+         *
          * @return RebillID
          */
         public static function createRebillEvent (array $values = array()) {
-            
+
             // Find out the correct frequency values.
             $frequency = self::getFrequency($values['frequency']);
             $values['RebillInterval'] = $frequency['RebillInterval'];
             $values['RebillIntervalType'] = $frequency['RebillIntervalType'];
-            
+
             // Check for missing fields
             $valid_data = true;
             $missing_fields = array();
-            $error = null;                
+            $error = null;
             foreach (RecurringPaymentsSettings::getRequiredCreateRebillEvent() as $field_name) {
                 if (!array_key_exists($field_name, $values) || $values[$field_name] == '') {
                     $missing_fields[] = $field_name;
@@ -192,18 +192,18 @@
                     'missing-fields' => $missing_fields
                 );
             }
-            
+
             $eway_request_xml = simplexml_load_string('<CreateRebillEvent xmlns="http://www.eway.com.au/gateway/rebill/manageRebill" />');
             foreach($values as $field_name => $field_data) {
                 $eway_request_xml->addChild($field_name, General::sanitize($field_data));
             }
-            
+
             // Execute the transaction.
             $ch = Recurring_Request::start(RecurringPaymentsSettings::getGatewayURI(), $eway_request_xml->asXML());
 
             $curl_result = curl_exec($ch);
             $status = curl_getinfo($ch);
-            
+
             if (curl_errno($ch)) {
                 // The Gateway did not connect to eWay successfully or some error occurred.
                 return array(
@@ -213,14 +213,14 @@
                     'curl-info' => $info,
                     'raw' => $curl_result
                 );
-            } 
+            }
             else {
-                
+
                 curl_close($ch);
 
                 $dom = new DOMDocument();
                 $response = $curl_result;
-               
+
                 $dom->loadXML($response);
                 $xpath = new DOMXPath($dom);
 
@@ -233,7 +233,7 @@
 
                 $rebillID = $xpath->evaluate('string(/soap:Envelope/soap:Body//eway:RebillID)');
                 $result = $xpath->evaluate('string(/soap:Envelope/soap:Body//eway:Result)');
-                
+
                 // Return a new rebill customer id.
                 if ($result == "Success") {
                     return $rebillID;
@@ -245,28 +245,28 @@
                         'curl-info' => $status,
                         'raw' => $curl_result
                     );
-                }                    
-            }           
+                }
+            }
         }
-        
+
         /**
          * Update a Rebill Event.
-         * 
+         *
          * @param array $values
-         * 
+         *
          * @return boolean
          */
         public static function updateRebillEvent (array $values = array()) {
-            
+
             // Find out the correct frequency values.
             $frequency = self::getFrequency($values['frequency']);
             $values['RebillInterval'] = $frequency['RebillInterval'];
-            $values['RebillIntervalType'] = $frequency['RebillIntervalType'];            
-            
+            $values['RebillIntervalType'] = $frequency['RebillIntervalType'];
+
             // Check for missing fields
             $valid_data = true;
             $missing_fields = array();
-            $error = null;                
+            $error = null;
             foreach (RecurringPaymentsSettings::getRequiredUpdateRebillEvent() as $field_name) {
                 if (!array_key_exists($field_name, $values) || $values[$field_name] == '') {
                     $missing_fields[] = $field_name;
@@ -283,18 +283,18 @@
                     'missing-fields' => $missing_fields
                 );
             }
-            
+
             $eway_request_xml = simplexml_load_string('<UpdateRebillEvent xmlns="http://www.eway.com.au/gateway/rebill/manageRebill" />');
             foreach($values as $field_name => $field_data) {
                 $eway_request_xml->addChild($field_name, General::sanitize($field_data));
             }
-            
+
             // Execute the transaction.
             $ch = Recurring_Request::start(RecurringPaymentsSettings::getGatewayURI(), $eway_request_xml->asXML());
 
             $curl_result = curl_exec($ch);
             $status = curl_getinfo($ch);
-            
+
             if (curl_errno($ch)) {
                 // The Gateway did not connect to eWay successfully or some error occurred.
                 return array(
@@ -303,9 +303,9 @@
                     'response-message' => __('There was an error connecting to eWay.'),
                     'curl-info' => $info
                 );
-            } 
+            }
             else {
-                
+
                 curl_close($ch);
 
                 $dom = new DOMDocument();
@@ -317,40 +317,40 @@
                 $xpath->registerNamespace('eway', 'http://www.eway.com.au/gateway/rebill/manageRebill');
                 $xpath->registerNamespace('soap', 'http://schemas.xmlsoap.org/soap/envelope/');
                 $result = $xpath->evaluate('string(/soap:Envelope/soap:Body//eway:Result)');
-                
+
                 // Return a new rebill customer id.
                 if ($result == "Success") {
                     return true;
                 } else{
                     // Error from the API.
-                    $error = $xpath->evaluate('string(/soap:Envelope/soap:Body//eway:ErrorDetails)');                    
+                    $error = $xpath->evaluate('string(/soap:Envelope/soap:Body//eway:ErrorDetails)');
                     return array(
                         'status' => __('Gateway error'),
                         'response-code' => PGI_Response::GATEWAY_ERROR,
                         'response-message' => $error,
                         'curl-info' => $status
                     );
-                }                    
-            }           
-        } 
+                }
+            }
+        }
 
         /**
          * Delete existing rebill event.
-         * 
+         *
          * @param type $rebillCustomerID
          * @param type $rebillID
-         * 
+         *
          * @return boolean
          */
         public static function deleteRebillEvent($rebillCustomerID, $rebillID) {
-            
+
             if (!$rebillCustomerID) return;
             if (!$rebillID)         return;
-            
+
             $eway_request_xml = simplexml_load_string('<DeleteRebillEvent xmlns="http://www.eway.com.au/gateway/rebill/manageRebill" />');
             $eway_request_xml->addChild('RebillCustomerID', General::sanitize($rebillCustomerID));
             $eway_request_xml->addChild('RebillID', General::sanitize($rebillID));
-            
+
             // Execute the transaction.
             $ch = Recurring_Request::start(RecurringPaymentsSettings::getGatewayURI(), $eway_request_xml->asXML());
 
@@ -365,9 +365,9 @@
                     'response-message' => __('There was an error connecting to eWay.'),
                     'curl-info' => $info
                 );
-            } 
+            }
             else {
-                
+
                 curl_close($ch);
 
                 $dom = new DOMDocument();
@@ -379,7 +379,7 @@
                 $xpath->registerNamespace('eway', 'http://www.eway.com.au/gateway/rebill/manageRebill');
                 $xpath->registerNamespace('soap', 'http://schemas.xmlsoap.org/soap/envelope/');
                 $result = $xpath->evaluate('string(/soap:Envelope/soap:Body//eway:Result)');
-                
+
                 // Return a boolean in case of success.
                 if ($result == "Success") {
                     return true;
@@ -390,28 +390,28 @@
                         'response-message' => __('There was an error updating a reBill event.'),
                         'curl-info' => $status
                     );
-                }  
+                }
 
-            }  
-        }          
-        
+            }
+        }
+
         /**
          * Query transaction details for specified rebill event. StartDate, EndDate and Status are optional values.
-         * 
+         *
          * @param type $rebillCustomerID
-         * @param type $rebillID         
-         * 
+         * @param type $rebillID
+         *
          * @return \RecurringPaymentsResponse|array
-         */        
-        public static function queryTransactions ($rebillCustomerID, $rebillID) {            
+         */
+        public static function queryTransactions ($rebillCustomerID, $rebillID) {
 
             if (!$rebillCustomerID) return;
             if (!$rebillID)         return;
-            
+
             $eway_request_xml = simplexml_load_string('<QueryTransactions xmlns="http://www.eway.com.au/gateway/rebill/manageRebill" />');
             $eway_request_xml->addChild('RebillCustomerID', General::sanitize($rebillCustomerID));
             $eway_request_xml->addChild('RebillID', General::sanitize($rebillID));
-            
+
             // Execute the transaction.
             $ch = Recurring_Request::start(RecurringPaymentsSettings::getGatewayURI(), $eway_request_xml->asXML());
 
@@ -426,9 +426,9 @@
                     'response-message' => __('There was an error connecting to eWay.'),
                     'curl-info' => $info
                 );
-            } 
+            }
             else {
-                
+
                 curl_close($ch);
 
                 $dom = new DOMDocument();
@@ -441,40 +441,40 @@
                 $xpath->registerNamespace('soap', 'http://schemas.xmlsoap.org/soap/envelope/');
 
                 $QueryTransactionsResult = $xpath->query('/soap:Envelope/soap:Body//eway:QueryTransactionsResult');
-                
+
                 // Build an array with old/future transactions.
                 $queries = array();
                 foreach($QueryTransactionsResult as $node) {
                     foreach ($node->childNodes as $child) {
                         foreach ($child->childNodes as $a) {
-                            $values[$a->nodeName] = $a->nodeValue; 
+                            $values[$a->nodeName] = $a->nodeValue;
                         }
                         array_push($queries, $values);
                     }
                 }
-                
+
                 // Return an array with the transactions.
                 return $queries;
-            } 
-        } 
-        
+            }
+        }
+
         /**
          * Query the next transaction existing rebill event.
-         * 
+         *
          * @param type $rebillCustomerID
          * @param type $rebillID
-         * 
+         *
          * @return array
          */
         public static function queryNextTransaction ($rebillCustomerID, $rebillID) {
-            
+
             if (!$rebillCustomerID) return;
             if (!$rebillID)         return;
-            
+
             $eway_request_xml = simplexml_load_string('<QueryNextTransaction xmlns="http://www.eway.com.au/gateway/rebill/manageRebill" />');
             $eway_request_xml->addChild('RebillCustomerID', General::sanitize($rebillCustomerID));
             $eway_request_xml->addChild('RebillID', General::sanitize($rebillID));
-            
+
             // Execute the transaction.
             $ch = Recurring_Request::start(RecurringPaymentsSettings::getGatewayURI(), $eway_request_xml->asXML());
 
@@ -489,9 +489,9 @@
                     'response-message' => __('There was an error connecting to eWay.'),
                     'curl-info' => $info
                 );
-            } 
+            }
             else {
-                
+
                 curl_close($ch);
 
                 $dom = new DOMDocument();
@@ -509,7 +509,7 @@
                 $Amount = $xpath->evaluate('string(/soap:Envelope/soap:Body//eway:Amount)');
                 $Status = $xpath->evaluate('string(/soap:Envelope/soap:Body//eway:Status)');
                 $Type = $xpath->evaluate('string(/soap:Envelope/soap:Body//eway:Type)');
-                
+
                 // Return an array with the next transaction details.
                 return array(
                     'TransactionDate' => $TransactionDate,
@@ -520,12 +520,12 @@
                     'Type' => $Type
                 );
 
-            }  
+            }
         }
-        
+
         /**
          * Setup the correct fields for eWay.
-         * 
+         *
          * @param type $type    Type of payment: Weekly, Fortnightly, Montly or Yearly.
          * @return int
          */
@@ -549,8 +549,8 @@
                     break;
             }
             return $data;
-        }  
-        
+        }
+
     }
 
     Class RecurringPaymentsResponse extends eWayResponse {
